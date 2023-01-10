092b01fd3a15bb237f351cd9559d404773af90be
Bryce Corbett <brycecorbett2@gmail.com>
2023-01-10 11:35:21 -0600

    progress

--- acer_qpcr.R
+++ acer_qpcr.R
@@ -2,10 +2,12 @@ library(steponeR)
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+library(lubridate)
 p2 <- list.files(path = "qPCR", pattern = ".txt", full.names = TRUE)
 p2
 
 master<-read.csv(header = TRUE, file ="acer_master.csv")
+
 df <- steponeR(files = p2, 
               delim = "\t", 
               target.ratios = c("Sym.Cam"),
@@ -19,11 +21,21 @@ acer<-acer %>%
   filter(!Sample.Name== "positive",!Sample.Name == "negative") %>%
   filter(!Sample.Name == "neg", !Sample.Name == "pos", !Sample.Name == "NEC6")%>%
   filter(!Sample.Name == "277", !Sample.Name == "279")
-
+nrow(acer)
 #convets numeric timepoint in master sheet to character in order to combine them across the qpcr data
 master<-master%>%mutate_if(is.numeric,as.character)%>%
     full_join(acer)
     #select(master,Sample.Name,TimePoint,Cam.CT.mean,Sym.CT.mean,Sym.Cam,Sym.CT.sd,Cam.CT.sd)
-test<-select(master,Sample.Name,TimePoint,Cam.CT.mean,Sym.CT.mean,Sym.Cam,Sym.CT.sd,Cam.CT.sd )
+final<-select(master,Sample.Name,TimePoint,Cam.CT.mean,Sym.CT.mean,Sym.Cam,Sym.CT.sd,Cam.CT.sd )
+final<-transform(final, TimePoint = as.numeric(TimePoint)) #converts Timpoint which was originally a "character" type to numeric type
 
-    
+master<-master %>%
+  mutate(Date = as_date(Date,format = "%m/%e/%y"))
+
+
+
+ggplot(master, aes(x = Date, y = log10(Sym.Cam), color = Treatment, group = interaction(Date,Treatment)))+
+  geom_point()+
+  geom_violin()
+
+str(master)
